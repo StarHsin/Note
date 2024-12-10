b57b6bd3e926eadf40d9a122927b59e762d6380b
star <otzhelen2018@gmail.com>
2024-12-10 20:36:36 +0800

    feat:將畫布置中

--- src/DrawingBoard.jsx
+++ src/DrawingBoard.jsx
@@ -1,14 +1,14 @@
 import { ReactSketchCanvas } from "react-sketch-canvas";
 import { useState, useEffect } from "react";
 
-function App() {
+function Test() {
   const [canvasSize, setCanvasSize] = useState({ width: 600, height: 600 });
 
   useEffect(() => {
     // 定義一個 resize 事件處理函數
     const handleResize = () => {
       const newWidth = window.innerWidth * 0.9;  // 寬度設為視窗寬度的 80%
-      const newHeight = window.innerHeight * 0.8; // 高度設為視窗高度的 60%
+      const newHeight = window.innerHeight * 0.75; // 高度設為視窗高度的 60%
       setCanvasSize({ width: newWidth, height: newHeight });
     };
 
@@ -37,4 +37,4 @@ function App() {
   );
 }
 
-export default App;
+export default Test;
